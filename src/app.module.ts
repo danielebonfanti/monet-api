1d9f6567bcc0fe76a2d12a03eae92ac728f3583e
Daniele Bonfanti <bonfantidaniele94@gmail.com>
2021-10-07 14:00:38 +0200

    Update mongo db connection, hide password in env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { StoriesModule } from './stories/stories.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://daniele_admin:eDvDbrCip7qXMaHJ@cluster0.m4vq9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
     StoriesModule,
+    ConfigModule.forRoot(),
   ],
 })
 export class AppModule {}
