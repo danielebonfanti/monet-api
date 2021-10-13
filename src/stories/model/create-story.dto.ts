56a0d40517a0d5180f3c1bec0069c96923a6c839
Daniele Bonfanti <bonfantidaniele94@gmail.com>
2021-10-13 09:07:48 +0200

    Add validation for createStory

--- src/stories/model/create-story.dto.ts
+++ src/stories/model/create-story.dto.ts
@@ -1,6 +1,11 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateStoryDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   text: string;
-  preview: string;
+
   tag: string[];
 }
