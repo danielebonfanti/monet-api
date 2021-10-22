5983deab5cbfee212322ac6a10388d1d4d0000e9
Daniele Bonfanti <bonfantidaniele94@gmail.com>
2021-10-22 19:28:30 +0200

    Rimosso commento

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { LocalStrategy } from './local.strategy';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '15m' }, //TODO: set correct time
+      signOptions: { expiresIn: '15m' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
